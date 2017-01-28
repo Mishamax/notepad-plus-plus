b64448a33c86d44e46a2b31b97c6534c1f547dfe
Michael Maximov <michael.maximov@gmail.com>
2017-01-28 18:47:18 +0300

    Added distr.bat script for creating a distribution of Notepad++ in a folder

---
+++ PowerEditor/distr.bat
@@ -0,0 +1,25 @@
+
+@rem Creates a distribution of Notepad++ in the specified folder
+@echo off
+
+set /p DISTR="Enter distribution folder: "
+set DISTR=%DISTR%\Notepad++
+if not exist "%DISTR%" mkdir "%DISTR%"
+
+copy "..\scintilla\bin\SciLexer.dll" "bin"
+
+pushd bin
+copy "allowAppDataPlugins.xml" "%DISTR%"
+copy "change.log"              "%DISTR%"
+copy "contextMenu.xml"         "%DISTR%"
+copy "doLocalConf.xml"         "%DISTR%"
+copy "functionList.xml"        "%DISTR%"
+copy "langs.model.xml"         "%DISTR%"
+copy "license.txt"             "%DISTR%"
+copy "notepad++.exe"           "%DISTR%"
+copy "readme.txt"              "%DISTR%"
+copy "SciLexer.dll"            "%DISTR%"
+copy "shortcuts.xml"           "%DISTR%"
+copy "stylers.model.xml"       "%DISTR%"
+popd
+
